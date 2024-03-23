2e3116be8207f53d8ecb2a79838e41209d12cde9
mirolim725 <mirolimsherboboyev@gmail.com>
2024-03-23 13:49:10 +0300

    first commit

--- app.js
+++ app.js
@@ -1,399 +1,191 @@
 
-// FOR-1
-
-// let k=5,
-// n = 3;
+'use strict'
 
 
+// IF-12
 
-// for (let i = 0; i < n ; i++) {
-//     console.log(k);
+
+// let a = 3, b = 6,c = 7;
+
+
+// if (a < b && a < c  ) {
+//     console.log(a);
+// } else if (b < c && b < a){
+//     console.log(b);
+// } else if (c < a && c <b) {
+//     console.log(c);
+// } else {
+//     console.log("Hammasi teng");
 // }
 
 
+// IF-13
 
-// FOR-2
+// let a=11 , b=22 , c=33
 
-// let a = 12 , b = 5 , count=0 ;
+// if (a < b && a > c){
+//     console.log(a);
+// }else if (b < c && b >a){
+//     console.log(b);
+// } else if (c < a && c > b) {
+//     console.log(c);
+// }else {
+//     console.log('Hammasi teng');
+// }
 
-// if (a < b) {
-//     for (let i = a ; i <= b; i++) {
-//         console.log(i);
-//         count++
-//     }
+
+// IF-14
+
+// let a=2 , b=10, c=10;
+
+// if (a < b && a < c && b < c) {
+//     console.log(a,b,c);
+// } else if (a <= b && a <= c && b >= c) {
+//     console.log(a,c,b);
+// } else if (b <= a && b <= c && a <= c) {
+//     console.log(b,a,c);
+// } else if (b <= a && b <= c && a >= c) {
+//     console.log(b,c,a);
+// } else if (c <= a && c <= b && a >= b) {
+//     console.log(c,a,b);
+// } else if (c <= a && c <= b && b >= a) {
+//     console.log(c,b,a);
+// } else {
+//     console.log('Xato');
+// }
+
+// IF-15
+
+// let x=1 , y=2 , z=3;
+
+// let a=x+y , b=x+z , c=y+z;
+
+
+// if (a > b && a > c  ) {
+//     console.log(a);
+// } else if (b > c && b > a){
+//     console.log(b);
+// } else if (c > a && c > b) {
+//     console.log(c);
+// } else {
+//     console.log("Hammasi teng");
+// }
+
+// IF-16
+
+// let a=0 , b=5, c=6;
+
+// if (a < b && a < c && b < c ) {
+//     console.log(a*2,b*2,c*2);
+// }else {
+//     console.log(`-${a} , -${b} , -${c}`);
+// }
+
+// IF-17
+
+// let a=10 , b=9, c=8;
+
+// if (a > b && a > c && b > c ) {
+//     console.log(a*2,b*2,c*2);
+// }else {
+//     console.log(`-${a} , -${b} , -${c}`);
+// }
+
+// 18-IF
+
+// let a=1, b=2, c=1 ;
+// if (a===b && c!=(a&&b)) {
+//     console.log("3")
+// }else if(a===c && b!=(a&&c)){
+//     console.log("2");
+// }else if(b===c && a!=(c&&b)){
+//     console.log("1");
+// }else{
+//     console.log('Ikkita teng son mavjud emas');
+// }
+
+
+// IF-19
+
+
+// let a = prompt("a" , '')
+// let b = prompt("b" , '')
+// let c = prompt("c" , '')
+// let d = prompt("d" , '')
+
+// if (a===b && b===c && d!=(a&&b&&c)) {
+//     alert("1");
+// }else if(a===b && b===d && c!=(a&&b&&d)){
+//     alert("2");
+// }else if(a===c && c===d && b!=(c&&b&&a)){
+//     alert("3");
+// }else if(b===c && c===d && a!=(c&&b&&d)){
+//     alert("4");
+// }else{
+//     alert('Uchta teng son mavjud emas');
+// }
+
+
+// IF-20
+
+// let a=1 , b=2 , c=3;
+
+// let x=b-a , y=c-a;
+
+// if (x<y) {
+//     console.log(x);
+// } else if(y<x){
+//     console.log(y);
 // } else{
-//     for (let i = b; i <= a; i++) {
-//         console.log(i);
-//         count++
-//     }
-// }
-// console.log(count);
-
-
-
-
-// FOR-3
-
-
-// let a = 1 , b = 5 , count=0 ;
-
-// if (a < b) {
-//     for (let i = b - 1  ; i > a; i--) {
-//         console.log(i);
-//         count++
-//     }
-// } else{
-//     for (let i = a - 1 ; i > b; i--) {
-//         console.log(i);
-//         count++
-//     }
-// }
-// console.log(count);
-
-
-
-// FOR-4
-
-
-
-// let a = 1 , b = 10 , c = 100 ;
-
-// for (let i = a ; i <= b ; i++) {
-//     let narx = i * c
-//     console.log(`${i} kilogramm konfetning narxi ${narx} so'm`);
+//     console.log('Togri son kiriting');
 // }
 
 
-// FOR-5
+// IF-21
 
+// let x=1 , y=1;
 
-// let a = 1 , b = 10, c = 100 ;
-
-// for (let i = a ; i <= b ; i++ ) {
-//     let narx = (i / 10) * c
-//     console.log(`${i / 10} kilogramm konfetning narxi ${narx} so'm`);
+// if (x===0 && y===0) {
+//     console.log('0');
+// }else if(y===0){
+//     console.log('1');
+// }else if(x===0){
+//     console.log('2');
+// }else{
+//     console.log('3');
 // }
 
+// IF-22
 
+// let x=1 , y=-1;
 
-// FOR-6
-
-
-// let a = 10 , b = 20, c = 100 ;
-
-// for (let i = a ; i <= b ; i+=2 ) {
-//     let narx = (i / 10) * c
-//     console.log(`${i / 10} kilogramm konfetning narxi ${narx} so'm`);
+// if (x > 0 && y > 0) {
+//     console.log('Birinchi chorak');
+// }else if (x < 0 && y > 0) {
+//     console.log('Ikkinchi chorak');
+// }else if (x < 0 && y < 0) {
+//     console.log('Uchinchi chorak');
+// }else if (x > 0 && y < 0) {
+//     console.log(`Tortinchi chorak`);
+// }else{
+//     console.log('Error');
 // }
 
+// IF-23
 
-// FOR-7
+// let x1=1 , y1=1;
+// let x2=1 , y2=2;
+// let x3=2 , y3=2;
 
-
-
-
-// let a = 1 , b = 100 , count=0 ;
-
-// if (a < b) {
-//     for (let i = a ; i <= b; i++) {  
-//         count+=i
-//     }
-// } else{
-//     for (let i = b; i <= a; i++) {
-//         count+=i  
-//     }
-// }
-// console.log(count);
-
-
-
-
-// FOR-8
-
-
-
-
-// let a = 1 , b = 100 , count=1 ;
-
-// if (a < b) {
-//     for (let i = a ; i <= b; i++) {  
-//         count*=i
-//     }
-// } else{
-//     for (let i = b; i <= a; i++) {
-//         count*=i  
-//     }
-// }
-// console.log(count);
-
-
-
-
-// FOR-9
-
-
-
-
-// let a = 1 , b = 4 , count=0 ;
-
-// if (a < b) {
-//     for (let i = a ; i <= b; i++) {  
-//         let result= i*i
-//         count+=result
-//     }
-// } else{
-//     for (let i = b; i <= a; i++) {
-//         let result= i*i
-//         count+=result
-//     }
-// }
-// console.log(count);
-
-
-
-// FOR-10
-
-
-
-
-// let n = 2 , count = 0;
-
-// for (let i = 1; i <= n; i++) {
-//     count+=1/i
-// }
-// console.log(count);
-
-
-
-// FOR-11
-
-
-// let n = 1 ;
-// let total = 0;
-// for (let i = n; i <= 2 * n; i++) {
-//      total += i ** 2;
-// }
-// console.log(total);
-
-
-
-// FOR-12
-
-// let n = 3;
-// let result = 1.1;
-// for (let i = 1.1; i <= n; i += 1) {
-//     result *= i;
-// }
-// console.log(result);
-
-
-// FOR-13
-
-// let n = 3 , a = 0.0 , b = 1 , c = 1.1;
-
-// for (let i = 1; i <= n; i++) {
-//     a += b * c;
-//     c += 0.1;
-//     b *= -1;
+// if((x1&&x2&&x3&&y1&&y2&&y3)>0){
+//     console.log(x3,y1, `va ular birinchi chorakda`);
+// } else if((x1&&x2&&x3)<0 && (y1&&y2&&y3)>0){
+//     console.log(x3,y1 ,`va ular ikkinchi chorakda`);
+// }  else if((x1&&x2&&x3)<0 && (y1&&y2&&y3)<0){
+//     console.log(x3,y1 ,`va ular uchinchi chorakda`);
+// }  else if((x1&&x2&&x3)>0 && (y1&&y2&&y3)<0){
+//     console.log(x3,y1 ,`va ular to'rtinchi chorakda`);
+// }  else {
+//     console.log('Nuqtalar xato');
 // }
 
-// console.log(a);
-
-
-
-// FOR-14
-
-
-// let n = 7 , count = 1;
-
-
-// for (let i = 3; i <= n; i+=2) {
-//     console.log(`${count} + ${i}`);
-//     count += i
-// }
-
-// console.log(Math.pow(count, 2));
-
-
-// FOR-15
-
-
-// let a = 2 , b = 3 ;
-// for (let i = 1; i <= 1; i++) {
-//     a **= b;
-// }
-// console.log(a);
-
-
-
-
-// FOR-16
-
-
-// let a = 3, b = 3 ;
-
-// for (let i = 0; i <= b; i++) {
-//    c = a**i
-//    console.log(c)
-// }
-
-
-
-// FOR-17
-
-
-
-// let a = 5, b = 2 , c = 0;
-
-// for (let i = 0; i <= b; i++) {
-//    c += Math.pow(a,i)
-//    console.log("c="+c);
-// }
-// console.log(c);
-
-
-
-
-// FOR-18
-
-// let a = 2, b = 2, c = 0;
-
-// for (let i = 0; i <= b; i++) {
-//    c += a ** i ;
-//    console.log("c="+c);
-//    a *= -1;
-// }
-// console.log(c);
-
-// FOR-19
-
-// let n = 5 , count = 1;
-
-// for (let i = 1; i <= n; i++) {
-//    count *= i
-//    console.log(count);
-// }
-
-
-
-// FOR-20
-
-// let n = 4 , count = 0;
-
-// for (let i = 1; i <= n; i++) {
-//    let x = 1;  
-//    for (let j = 1 ; j <= i; j++) {
-//       x*=j
-//    }
-//    count += x
-// }
-// console.log(count);
-
-
-
-// FOR-21
-
-
-
-// let n = 4 , count = 0;
-
-// for (let i = 1; i <= n; i++) {
-//    let x = 1;  
-//    for (let j = 1 ; j <= i; j++) {
-//       x*=j
-//    }
-//    count += 1/x
-// }
-// console.log(count);
-
-
-
-// FOR-22
-
-
-// let n = 4 , count = 0;
-
-// for (let i = 1; i <= n; i++) {
-//    let x = 2;
-//    x **= i;  
-//    for (let j = 1 ; j <= i; j++) {
-//       x*=j
-//    }
-//    count += 1/x
-   
-// }
-// console.log(count);
-
-
-// FOR-23
-
-
-
-// let n = 5 , count = 0 , sum = 0;
-
-// for (let i = 1; i <= n; i+=2) {
-//    let x = 2;
-//    x **= i;
-//    sum = 1;
-//    for (let j = 1 ; j <= i; j++) {
-//       sum*=j
-//    }
-//    count += x/sum
-//    x *=-1
-//    console.log(x);
-// }
-// console.log(count);
-
-
-// FOR-24
-
-
-
-
-// let n = 5 , count = 0 , sum = 0;
-
-// for (let i = 0; i <= n; i+=2) {
-//    let x = 2;
-//    x **= i;
-//    sum = 1;
-//    for (let j = 1 ; j <= i; j++) {
-//       sum*=j
-//    }
-//    count += x/sum
-//    x *=-1
-// }
-// console.log(count);
-
-
-// FOR-25
-
-
-// let n = 5 , count = 0 , sum = 0;
-
-// for (let i = 1; i <= n; i++) {
-//    let x = 2;
-//    x **= i;
-//    sum = 1;
-//    count += x/i
-//    x *=-1
-//    console.log(x);
-// }
-// console.log(count);
-
-
-
-// FOR-26
-
-
-
-// let n = 5 , count = 0 , sum = 0;
-
-// for (let i = 1; i <= n; i+=2) {
-//    let x = 2;
-//    x **= i;
-//    sum = 1;
-//    count += x/i
-//    x *=-1
-//    console.log(i);
-// }
-// console.log(count);
